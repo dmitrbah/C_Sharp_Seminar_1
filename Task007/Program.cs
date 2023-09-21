613f436db160c72032fff8b78470b329151d57d7
Dmitry <dmitrbah@mail.ru>
2023-09-21 15:13:55 +0300

    Added Task007

---
+++ Task007/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
+if (number >= 100 && number <= 999)
+{
+    int lastDigit = number % 10;
+    Console.WriteLine($"Последняя цифра числа {number} -> {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
