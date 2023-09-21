38d3a52d67dde20e8cf808dd3382fd42a2197027
Dmitry <dmitrbah@mail.ru>
2023-09-21 14:41:54 +0300

    Added Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число дня недели: ");
+string WeekDay = Console.ReadLine();
+if (WeekDay == "1")
+    Console.WriteLine($"{WeekDay} -> Понедельник");
+else if (WeekDay == "2")
+    Console.WriteLine($"{WeekDay} -> Вторник");
+else if (WeekDay == "3")
+    Console.WriteLine($"{WeekDay} -> Среда");
+else if (WeekDay == "4")
+    Console.WriteLine($"{WeekDay} -> Четверг");
+else if (WeekDay == "5")
+    Console.WriteLine($"{WeekDay} -> Пятница");
+else if (WeekDay == "6")
+    Console.WriteLine($"{WeekDay} -> Суббота");
+else if (WeekDay == "7")
+    Console.WriteLine($"{WeekDay} -> Воскресенье");
+else
+    Console.WriteLine("Неправильно введен день недели");
